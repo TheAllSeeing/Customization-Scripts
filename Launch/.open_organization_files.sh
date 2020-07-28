26a2f6d0bec750d7403c73efc35aada6e5cdfa62
Atai Ambus <atayambus@gmail.com>
2020-07-28 15:38:51 +0300

    fix(launch_rule): fix broken path, add debugging.
    - Changed report template path from relative to absolute, since the
    relative path was from a different directory.
    
    - Added some debugging throughout the code, in conditionals and some of
    the general actions.

--- Launch/.open_organization_files.sh
+++ Launch/.open_organization_files.sh
@@ -1,12 +1,14 @@
-#!/bin/env bash
+#!/bin/bash
 cd ~/Code-Steel\ \(ORG\)/FORMALIZE.proj
 
 report="Progress Reports/$(date +%m\ %B)/$(date +%d,\ %A)"
 yesterday_report="Progress Reports/$(date -d 'yesterday' +%m\ %B)/$(date -d 'yesterday' +%d,\ %A)"
 home="../../"
 yesterday_output="$home/Code-White (INTRA)/RECORD.rou/Output/$(date -d 'yesterday' +%m\ %B)/$(date -d 'yesterday' +%d,\ %A)"
+
 if [ ! -f "$report.lyx" ]; then
     cat ~/.lyx/templates/Report.lyx > "$report.lyx"
+    echo created report
 
     case $(date +%d | tail -c2) in
         1)
@@ -22,8 +24,12 @@ if [ ! -f "$report.lyx" ]; then
             suffix="th"
     esac
 
-    sed -i "$(awk '/June 29/{print NR; exit}' .lyx/templates/Report.lyx) c\ $(date +%A,\ %B\ %d$suffix,\ %Y)" "$report.lyx"
+    sed -i "$(awk '/June 29/{print NR; exit}' ~/.lyx/templates/Report.lyx) c\ $(date +%A,\ %B\ %d$suffix,\ %Y)" "$report.lyx"
+    echo named date
     lyx -e pdf "$report.lyx"
+    echo compiled pdf
+else
+    echo file exists
 fi
 
 lyx -r "$report.lyx"  Classify/ObjectClasses.lyx Lawbook/Lawbook.lyx Lawbook/Pending\ Enactment.lyx Lawbook/Pending\ Approval.lyx &
